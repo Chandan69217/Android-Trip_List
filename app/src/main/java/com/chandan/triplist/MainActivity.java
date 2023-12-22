49f8a3f06f9aeba6256f36d3776058154c5e6f70
Chandan Sharma <chandansharma69217@gmail.com>
2023-12-22 22:58:54 +0530

    search feature add

--- app/src/main/java/com/chandan/triplist/MainActivity.java
+++ app/src/main/java/com/chandan/triplist/MainActivity.java
@@ -1,10 +1,21 @@
 package com.chandan.triplist;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.viewpager.widget.ViewPager;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -25,5 +36,70 @@ public class MainActivity extends AppCompatActivity {
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.seearch,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.searchView);
+        SearchView searchView  = (SearchView) MenuItemCompat.getActionView(menuItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+               int index = 0,flag = 0;
+                for(DataModel obj : DataBase.getData()){
+                    index++;
+                    if(obj.getRoll().equalsIgnoreCase(s)){
+                        flag = 1;
+                        break;
+                    }
+                }
+                if(flag == 1){
+                    ListFrag.getRecyclerView().scrollToPosition(index - 1);
+                }else {
+                    AlertDialog.Builder alertDialog = new AlertDialog.Builder(getApplicationContext());
+
+                    alertDialog.setTitle("Search")
+                            .setMessage("Given roll No not found")
+                            .setIcon(R.drawable.baseline_add_alert_24)
+                            .setCancelable(false)
+                            .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.cancel();
+                                }
+                            }).show();
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        SearchView searchView = (SearchView) item.getActionView();
+//        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+//            @Override
+//            public boolean onQueryTextSubmit(String s) {
+//                return false;
+//            }
+//
+//            @Override
+//            public boolean onQueryTextChange(String s) {
+//                return false;
+//            }
+//        });
+        return super.onOptionsItemSelected(item);
     }
 }
